1f160cb019b0013e7b55f7acf10efb0523f591e1
Hoi Hin Adrian Ip <dreamresearcher0204@gmail.com>
2026-07-10 01:09:53 +0800

    house pricing v3

--- Housing Prices Competition for Kaggle Learn Users/main.py
+++ Housing Prices Competition for Kaggle Learn Users/main.py
@@ -8,6 +8,7 @@ from sklearn.pipeline import Pipeline
 
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
 
 from sklearn.compose import ColumnTransformer
 
@@ -25,15 +26,33 @@ X = data.drop(['SalePrice','Id'], axis=1)
 
 X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=0.8, random_state=42)
 
-num_col, cat_col = [], []
+ordinal_orders = {
+    "PoolQC": ['Na', 'Fa', 'TA', 'Gd', 'Ex'],
+    "GarageCond": ['Na', 'Po', 'Fa', 'TA', 'Gd', 'Ex'],
+    "GarageQual": ['Na', 'Po', 'Fa', 'TA', 'Gd', 'Ex'],
+    "FireplaceQu": ['Na', 'Po', 'Fa', 'TA', 'Gd', 'Ex'],
+    "KitchenQual": ['Po', 'Fa', 'TA', 'Gd', 'Ex'],
+    "HeatingQC": ['Po', 'Fa', 'TA', 'Gd', 'Ex'],
+    "BsmtCond": ['Na', 'Po', 'Fa', 'TA', 'Gd', 'Ex'],
+    "BsmtQual": ['Na', 'Po', 'Fa', 'TA', 'Gd', 'Ex'],
+    "ExterQual": ['Po', 'Fa', 'TA', 'Gd', 'Ex'],
+    "ExterCond": ['Po', 'Fa', 'TA', 'Gd', 'Ex'],
+}
+
+
+num_col, cat_col, ord_col = [], [], []
 for col, dtype in X.dtypes.items():
     if dtype == 'int64' or dtype == 'float64':
         num_col.append(col)
+    elif col in ordinal_orders:
+        ord_col.append(col)
     else:
         cat_col.append(col)
 
+ordinal_categories = [ordinal_orders[col] for col in ord_col]
+
 num_pipe = Pipeline([
-    ('impute', SimpleImputer()),
+    ('impute', SimpleImputer())
 ])
 
 cat_pipe = Pipeline([
@@ -41,10 +60,16 @@ cat_pipe = Pipeline([
     ('OHE', OneHotEncoder(handle_unknown='ignore'))
 ])
 
+ord_pipe = Pipeline([
+    ('impute', SimpleImputer(strategy='most_frequent')),
+    ('ord_encode', OrdinalEncoder(categories=ordinal_categories,handle_unknown='use_encoded_value', unknown_value=-1))
+])
+
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', num_pipe, num_col),
-        ('cat', cat_pipe, cat_col)
+        ('cat', cat_pipe, cat_col),
+        ('ord', ord_pipe, ord_col)
     ]
 )
 
@@ -54,9 +79,9 @@ model = Pipeline([
 ])
 
 param_grid = {
-    'model__n_estimators' : [250, 300, 350],
-    'model__max_depth' : [15, 20, 25],
-    'model__min_samples_leaf': [2, 3]
+    'model__n_estimators' : [350, 400, 450],
+    'model__max_depth' : [23, 25, 27],
+    'model__min_samples_leaf': [2]
 }
 
 grid = GridSearchCV(
@@ -85,7 +110,7 @@ grid.fit(X, y)
 
 test_X = test.drop('Id', axis=1)
 
-pred = grid.best_estimator_.predict(test)
+pred = grid.best_estimator_.predict(test_X)
 
 submission = pd.DataFrame({
     'Id': test['Id'],
